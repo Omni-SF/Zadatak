fc61d2f945dc8b47748a22be3e65270db48a2aaa
Omni-SF <psivec@gmail.com>
2018-11-05 12:36:30 +0100

    Delete Vlasnici.js

--- models/Vlasnici.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-const Vlasnici = new Schema({
-    vlIme: {type: String},
-    vlPrezime: {type: String}
-});
-
-module.exports = mongoose.model('Vlasnici', Vlasnici);
